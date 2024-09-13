4021f3ea47dd65a55e2bfa848e0258cc5a59e466
Dmitry <dimitri.87@mail.ru>
2024-09-13 18:06:13 +0500

    Set Closure Generator 1

--- 4_kyu/Set_Closure_Generator.py
+++ 4_kyu/Set_Closure_Generator.py
@@ -3,21 +3,23 @@ import time
 
 def closure_gen(*s):
 
-    if not s:
+    try:
+
+        given_set = set(s)
+        new_set = set()
+        new_set.add(min(given_set))
+
+        while True:
+
+            yield max(new_set)
+
+            diff = {i*j for j in new_set for i in given_set}.union(given_set).difference(new_set)
+
+            new_set.add(min(diff))
+
+    except Exception:
+
         return
-
-    given_set = set(s)
-    new_set = set()
-    new_set.add(min(given_set))
-
-    while True:
-
-        yield max(new_set)
-
-        diff = {i*j for j in new_set for i in given_set}.union(given_set).difference(new_set)
-        if not diff:
-            return
-        new_set.add(min(diff))
 
 
 for val in closure_gen(1):
