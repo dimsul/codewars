13d0ce741b61be5182e37f614278c0a99d145c16
dima <dimitri.87@mail.ru>
2024-09-19 17:18:45 +0500

    unique in order

--- 6_kyu/Unique In Order.py
+++ 6_kyu/Unique In Order.py
@@ -1,5 +1,12 @@
-def unique_in_order(sequence: list):
-    new_seq = []
-    while sequence:
-        new_seq.append(sequence[0])
-        sequence.
+def unique_in_order(sequence):
+    if not sequence:
+        return []
+    new_seq = [sequence[0],]
+    for item in sequence:
+        if item != new_seq[-1]:
+            new_seq.append(item)
+
+    return new_seq
+
+
+print(unique_in_order('AAAABBBCCDAABBB'))
