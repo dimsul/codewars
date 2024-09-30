d8e8dab12e773bdb9da243c594fa781710370760
dima <dimitri.87@mail.ru>
2024-09-30 15:02:26 +0500

    добавление тестов

--- test_kata/kyu_6/test_unique_in_order.py
+++ test_kata/kyu_6/test_unique_in_order.py
@@ -1,12 +1,23 @@
 import pytest
 
-from kyu_6 import unique_in_order
+from kyu_6.unique_in_order import unique_in_order
 
 
 input_params = (('AAAABBBCCDAABBB', ['A', 'B', 'C', 'D', 'A', 'B']),
                 ('ABBCcAD', ['A', 'B', 'C', 'c', 'A', 'D']),
                 ([1, 2, 2, 3, 3], [1, 2, 3]),
-                ((1, 2, 2, 3, 3), [1, 2, 3]),)
+                ((1, 2, 2, 3, 3), [1, 2, 3]),
+                ("", []),
+                ([], []),
+                ((), []),
+                ("A", ["A"]),
+                (['A'], ['A']),
+                (('A',), ['A']),
+                ('AA', ['A']),
+                ('AAAABBBCCDAABBB', ["A", "B", "C", "D", "A", "B"]),
+                ("ABBCcA", ["A", "B", "C", "c", "A"]),
+                ([1, 2, 3, 3, -1], [1, 2, 3, -1]),
+                (["a", "b", "b", "a"], ["a", "b", "a"]))
 
 
 @pytest.mark.parametrize('sequence, exp_list', input_params)
