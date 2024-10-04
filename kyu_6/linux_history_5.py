527346df348fc85101ca17ebb4e719d3db3b9825
dima <dimitri.87@mail.ru>
2024-10-04 15:54:53 +0500

    refactoring

--- kyu_6/linux_history_5.py
+++ kyu_6/linux_history_5.py
@@ -27,6 +27,3 @@ def bang_contain_string(s, history):
         return result[-1].strip('\n').split('  ')[-1]
     else:
         return f'!{s}: event not found'
-
-
-print(bang_contain_string('ps auxww |', "   1  touch me\n 2  cd ..\n 3  more me\n 4  cd ..\n 5  chmod 000 me\n 6  more beer\n 7  lost\n 8  touch me\n 9  cd /pub\n 10  more me\n 11  cd ..\n 12  chmod 000 me\n 13  more beer\n 14  lost\n 15  touch me\n 16  cd /pub\n 17  more me\n 18  cd ..\n 19  chmod 000 me\n 20  more beer\n 21  lost\n 22  cd /pub"))
