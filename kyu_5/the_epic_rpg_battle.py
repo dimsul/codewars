d8dde67baa2c32d724e7f9d12cb319b260416cb8
dima <dimitri.87@mail.ru>
2024-11-11 17:20:45 +0500

    The epic RPG battle (2-nd commit)

--- kyu_5/the_epic_rpg_battle.py
+++ kyu_5/the_epic_rpg_battle.py
@@ -12,6 +12,7 @@ class Warrior:
         self.hp = 260
         self.power = 40
         self.buff_activ = False
+        self.name = 'Warrior'
 
     def normal_attack(self):
         return self.power
@@ -37,6 +38,7 @@ class Mage:
         self.hp = 200
         self.power = 50
         self.buff_active = False
+        self.name = 'Mage'
 
     def normal_attack(self):
         return self.power
@@ -60,6 +62,7 @@ class Assassin:
         self.hp = 235
         self.power = 30
         self.buff_active = 0
+        self.name = 'Assassin'
 
     def normal_attack(self):
         self.__check_buff()
@@ -89,21 +92,47 @@ class Battle:
     def __init__(self, player_1, player_2):
         self.player_1 = player_1
         self.player_2 = player_2
+        self.battle_in_action = True
 
     def play_turn(self, act_1, act_2):
-        # your code here
-        pass
+        if not self.battle_in_action:
+            return 'This battle is over!'
+        else:
+            self.__action(act_1, self.player_1, self.player_2)
+            if self.__check_winner():
+                return self.__check_winner()
+            self.__action(act_2, self.player_2, self.player_1)
+            if self.__check_winner():
+                return self.__check_winner()
+            return f'{self.player_1.name} HP {self.player_1.hp}, {self.player_2.name} HP {self.player_2.hp}'
+
+    def __check_winner(self):
+        if self.player_1.hp <= 0:
+            self.battle_in_action = False
+            return f'The {self.player_2.name} won! Remaining HP = {self.player_2.hp}'
+        elif self.player_2.hp <= 0:
+            self.battle_in_action = False
+            return f'The {self.player_1.name} won! Remaining HP = {self.player_1.hp}'
+
+        return None
+
+    @staticmethod
+    def __action(act, pl_1, pl_2):
+        if act.value == 1:
+            pl_1.buff()
+        elif act.value == 2:
+            act_res = pl_1.normal_attack()
+            pl_2.acc_dmg(act_res)
+        elif act.value == 3:
+            act_res = pl_1.special_attack()
+            pl_2.acc_dmg(act_res)
 
 
-assassin = Assassin()
-print(assassin.power)
-assassin.buff()
-print(assassin.power)
-print(assassin.normal_attack())
-print(assassin.power)
-print(assassin.normal_attack())
-print(assassin.power)
-print(assassin.special_attack())
-print(assassin.power)
-print(assassin.special_attack())
-print(assassin.power)
+plr_1 = Assassin()
+plr_2 = Mage()
+epic_battle = Battle(plr_1, plr_2)
+print(epic_battle.play_turn(Actions.BUFF, Actions.BUFF))
+print(epic_battle.play_turn(Actions.SPECIAL_ATTACK, Actions.SPECIAL_ATTACK))
+print(epic_battle.play_turn(Actions.NORMAL_ATTACK, Actions.NORMAL_ATTACK))
+print(epic_battle.play_turn(Actions.SPECIAL_ATTACK, Actions.SPECIAL_ATTACK))
+print(epic_battle.play_turn(Actions.NORMAL_ATTACK, Actions.NORMAL_ATTACK))
